3c4be8bdb5a7ac95f5d06c9cda3fd82e524bd5be
Anish Mishra <anishkumarc92@gmail.com>
2024-07-12 02:22:09 +0530

    Basic calculator

---
+++ script.js
@@ -0,0 +1,25 @@
+let input = document.querySelector('input')
+let button = document.querySelectorAll('button')
+
+let str = ""
+let arr = Array.from(button)
+arr.forEach(function(button){
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML === '='){
+            str = eval(str);
+            input.value = str;
+        }
+        else if(e.target.innerHTML === 'AC'){
+            str = "";
+            input.value = str;
+        }
+        else if(e.target.innerHTML === 'DEL'){
+            str = str.substring(0, str.length-1);
+            input.value = str;
+        }
+        else{
+            str += e.target.innerHTML;
+            input.value = str;
+        }
+    })
+})
